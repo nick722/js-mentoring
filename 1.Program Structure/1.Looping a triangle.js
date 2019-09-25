504ec4a3f1bf5864cc52d9b177d7569952ae91fb
Nikolai_Iakushchenko <nikolai_iakushchenko@epam.com>
2019-09-25 17:40:11 +0300

    WIP 1.Looping a triangle

---
+++ 1.Program Structure/1.Looping a triangle.js
@@ -0,0 +1,19 @@
+// Looping a triangle
+// Write a loop that makes seven calls to console.log to output the following
+// triangle:
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+//     37
+// It may be useful to know that you can find the length of a string by writing
+//     .length after it.
+//     let abc = "abc";
+// console.log(abc.length);
+// // → 3
+////////////////////////////////////////////////////////////////////////////////
+
+
